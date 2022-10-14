a40f9c19c050bc232f4e9fdbbd9519c92e7c3b24
avidalto <avidalto@hawk.iit.edu>
2022-10-14 21:50:27 +0000

    Add workflow.xml

--- run_job.sh
+++ run_job.sh
@@ -4,11 +4,11 @@ cp -r /pw/openfoam_runner /pw/jobs/00000
 cd /pw/jobs/00000
 
 bash main.sh \
-    --controller pw.conf \
     --def_file openfoam9-paraview56.def \
     --sif_file /home/__USER__/pworks/openfoam/openfoam9-paraview56.sif \
-    --chdir /home/__USER__/pworks/__job_number__ \
     --cases_json_file /home/__USER__/pworks/openfoam/cyclone-template/cyclone-cases.json \
+    --controller pw.conf \
+    --chdir /home/__USER__/pworks/__job_number__ \
     --partition compute \
     --time 06:00:00 \
     --numnodes 1 \
