9d86002a1336e60e7f1deda89a98ac4515a35443
avidalto <avidalto@hawk.iit.edu>
2022-10-17 14:31:48 +0000

    Remove case creation from bootstrap

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -5,6 +5,3 @@ of_image=__of_image__
 
 # Build singularity container if sif_file is not present
 bash ${bdir}/build_singularity.sh ${sif_file} ${bdir}/${of_image}.def
-
-# Create OpenFOAM cases:
-python ${bdir}/create_cases.py cases.json
