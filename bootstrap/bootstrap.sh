fd72ff13fef2c2ecd3182fbbe508c88f79d50a01
avidalto <avidalto@hawk.iit.edu>
2022-11-02 16:22:45 -0500

    Add install singularity bootstrap

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,5 +3,11 @@ bdir=$(dirname $0)
 sif_file=__sif_file__
 of_image=__of_image__
 
+# Install singularity if it does not exist:
+if [ -z $(which singularity) ]; then
+    echo "Installing singularity"
+    bash ${bdir}/install_singularity.sh
+fi
+
 # Build singularity container if sif_file is not present
 bash ${bdir}/build_singularity.sh ${sif_file} ${bdir}/${of_image}.def
