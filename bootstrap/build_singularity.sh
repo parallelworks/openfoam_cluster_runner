66e43246cdcbea22269c8783c8ff9869b0b1a14d
avidalto <avidalto@hawk.iit.edu>
2022-11-03 11:03:44 -0500

    Make sure singularity file's directory exists

--- bootstrap/build_singularity.sh
+++ bootstrap/build_singularity.sh
@@ -6,6 +6,7 @@ if [ -f "${sif_file}" ]; then
     echo "Singularuty file ${sif_file} already exists"
 else
     echo "Building singularity file"
+    mkdir -p $(dirname ${sif_file})
     # Sometimes root cant find singularity :s
     sudo $(which singularity) build ${sif_file} ${def_file}
 fi
