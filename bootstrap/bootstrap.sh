ca31e0724255c8e9d1f0b1f359d6e19c10b03baf
avidalto <avidalto@hawk.iit.edu>
2022-11-03 10:35:31 -0500

    Attempt to load singularity

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,6 +3,8 @@ bdir=$(dirname $0)
 sif_file=__sif_file__
 of_image=__of_image__
 
+module load singularity
+
 # Install singularity if it does not exist:
 if [ -z $(which singularity) ]; then
     echo "Installing singularity"
