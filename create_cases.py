bc2c2da37260cc04d0e62623b85039bfb74f8351
avidalto <avidalto@hawk.iit.edu>
2023-06-26 13:17:38 -0500

    Save a case.json file with the case parameters

--- create_cases.py
+++ create_cases.py
@@ -1,4 +1,4 @@
-import json, shutil, os, argparse
+import json, sys, shutil, os, argparse
 
 
 def read_args():
@@ -31,20 +31,29 @@ if __name__ == '__main__':
         ))
         shutil.copytree(os.path.expanduser(template_directory), case['directory'])
 
-        for fdict in case['files']:
+        for fi,fdict in enumerate(case['files']):
+            print(json.dumps(case, indent = 4))
             print('    Rewriting file {}'.format(fdict['path']))
             f = open(os.path.join(case['directory'], fdict['path']), mode='r')
             ftext = f.read()
             f.close()
-            for param in fdict['parameters']:
+            for pi, param in enumerate(fdict['parameters']):
                 placeholder = param['placeholder']
                 value = str(param['value'])
                 if placeholder in args:
                     value = args[placeholder].replace('___', ' ')
+                    case['files'][fi]['parameters'][pi]['placeholder'] = value
                 print('      Replacing placeholder <{}> with value <{}>'.format(placeholder, value))
                 ftext = ftext.replace(placeholder, value)
 
             f = open(os.path.join(case['directory'], fdict['path']), mode='w')
             f.write(ftext)
             f.close()
+        
+        # Save updated case JSON with the case parameters in each case directory
+        with open(os.path.join(case['directory'], 'case.json'), 'w') as f:
+            json.dump(case, f, indent = 4)
+        
+        # Remove cases.json with all the cases from each case directory
+        os.remove(os.path.join(case['directory'], 'cases.json')) 
 
